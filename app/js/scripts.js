e12bffce959d230ed57eb24252a7d955b50f3400
Louise Iyengar <lki@louiseiyengar.com>
2018-10-26 17:31:15 -0400

    completed homework

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,27 +1,43 @@
-document.addEventListener('click', decide, false);
+ document.addEventListener('click', decide, false);
 
 var betaContent = `
 <h2>In Beta</h2>
 <p>Information about the beta program.<p>
-<div class="closer" href="#0">
-<div class="closex">✖︎</div>
-</div>
 `
 
-var itContent = `
-<h2>Coming Soon</h2>
-<p>This feature coming soon.<p>
+var pickitContent = `
+<h2>Pick It is coming soon!</h2>
+<p>We will show you how to get the freshest ingredients for our recipes.<p>
+`
+
+var cookitContent = `
+<h2>Cook It is coming soon!</h2>
+<p>We will show you the <strong>very</strong> best ways to cook our recipes.<p>
+`
+
+var storeitContent = `
+<h2>Store It is coming soon!</h2>
+<p>We will show you how to preseve our recipes so they taste fresher longer.<p>
+`
+
+var closePop = `
 <div class="closer" href="#0">
 <div class="closex">✖︎</div>
 </div>
 `
 
 function decide() {
-    console.log(event.target);
     if (event.target.matches('.beta')) {
         makePopover(betaContent); 
-    } else if (event.target.matches('.it')) { 
-        makePopover(itContent); 
+    } else if (event.target.matches('.it')) {
+        var whichButton = event.target.parentElement.className;
+        if (whichButton == 'nav-pickit') {
+            makePopover(pickitContent);
+        } else if (whichButton == 'nav-cookit' ) {
+            makePopover(cookitContent);
+        } else {
+            makePopover(storeitContent);
+        }
     } else if (event.target.matches('.closex')) {
         destroyPopover()
     }
@@ -34,7 +50,7 @@ function makePopover(content) {
     var popover = document.createElement('div');
     popover.classList.add('betainfo');
   
-    popover.innerHTML = content; 
+    popover.innerHTML = content + closePop; 
     document.querySelector('body').append(popover);
   
     function destroyPopover() {
@@ -48,17 +64,3 @@ function destroyPopover() {
     var targetElem = document.querySelector('.betainfo')
     targetElem.remove();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
